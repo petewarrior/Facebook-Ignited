9c910d77a09c06ebe4212f4b26c50085e2b2f462
alfonso <alfonso@darktemptress>
2012-08-23 10:57:30 -0500

    Adding examples to the config file for fb_apptype config, to help prevent users from using wrong details.

--- application/config/fb_ignited.php
+++ application/config/fb_ignited.php
@@ -6,7 +6,7 @@
  * fb_secret	is the secret you recieved from dev panel
  * fb_canvas 	the value you put in 'Canvas Page' field in dev panel or the address of your app.
  * fb_apptype	set to either 'iframe' or 'connect' based on what platform your app is
- * 				is running on.
+ * 				is running on. See example below the config.
  * fb_auth		is the default authentications, '' is basic authentication
  */
 $config['fb_appid']		= 'application id';
@@ -14,3 +14,9 @@ $config['fb_secret']	= 'application secret';
 $config['fb_canvas']	= 'your-app-address-on-fb';
 $config['fb_apptype']	= 'iframe';
 $config['fb_auth']		= '';
+
+/**
+ * --- fb_apptype examples ---
+ * iframe		your-facebook-namespace 
+ * connect		www.your-connect-domain.com/subfolder/
+ */
